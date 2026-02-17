78b64f19bf26d18fbcf3bcb4671f30bb4131c2f9
Sergio Milla Catalan <smilla-c@car11s5.42barcelona.com>
2026-02-17 12:35:45 +0100

    actualizar archivos

---
+++ includes/push_swap.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smilla-c <smilla-c@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 09:48:35 by smilla-c          #+#    #+#             */
+/*   Updated: 2026/02/17 12:35:12 by smilla-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_node
+{
+	int	value;
+	struct s_node	*next;
+} t_node;i
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+//---DECLARACION DE STACKS GLOBALES--- //
+
+
+t_stack	a;
+t_stack	b;
+
+
+// --- FUNCIONES MAIN --- //
+
+
+void	check_invalid_args(int argc, char **argv);
+void	check_duplicates_args(int argc, char **argv);
+
+// --- FUNCIONES MOVES --- //
+void	push(t_node **from, t_node **to);
+void	swap(t_node **stack);
+void	rotate(t_node **stack);
+void	reverse_rotate(t_node **stack);
+
+int		is_sorted(t_node **stack);
+t_node	*create_node(int value);
+
+#endif
+
--- src/main.c
+++ src/main.c
@@ -6,8 +6,58 @@
 /*   By: smilla-c <smilla-c@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 13:13:57 by smilla-c          #+#    #+#             */
-/*   Updated: 2026/02/16 13:14:02 by smilla-c         ###   ########.fr       */
+/*   Updated: 2026/02/17 12:34:51 by smilla-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+void	check_invalid_args(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char 	*s;
+
+	i = 0;
+	while (i < argc)
+	{
+		*s = argv[i];
+		j = 0;
+		if (s[j] == '-' || s[j] == '+')
+			j++;
+		if (s[j] == '\0')
+			return (1);
+		while (s[j])
+		{
+			if (s[j] < '0' || s[j] > '9')
+				return (1);
+			j++;
+		}
+	}
+}
+
+void	check_duplicates_args(int argc, char **argv)
+{
+}
+
+int	main(int argc, char **argv)
+{
+	t_stack	a;
+	t_stack b;
+
+	if (argc < 2)
+		return (write(2, "Error\n", 6),1);
+	if (check_invalid_args(argc, argv))
+		return (write(2, "Error\n", 6),1);
+	if (check_duplicates_args(argc, argv))
+		return (write(2, "Error\n", 6),1);
+
+	init_stack(&a, argc, argv);
+	init_stack(&b, 0, NULL);
+
+	select_strategy(&a, &b, argc, argv);
+
+	free_stack(&a);
+	free_stack(&b);
+	return (0);
+}
